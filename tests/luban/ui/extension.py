be42c95ee676a583fa73c6c3196449c8b1f531f4
Jiao Lin <linjiao@caltech.edu>
2011-08-17 12:02:19 -0700

    ed:   tests/luban/ui/extension.py: a bit of docs

--- tests/luban/ui/extension.py
+++ tests/luban/ui/extension.py
@@ -13,14 +13,27 @@
 
 
 from luban import ui as lui
+try:
+    lui.e.form
+except AttributeError:
+    pass
+else:
+    m = "form should not have been defined"
+    raise RuntimeError(m)
+
+# extension kicks in
 import extension_example
+# and form is available
+lui.e.form
 
 
 import unittest
 class TestCase(unittest.TestCase):
 
     def test1(self):
+        # create a form
         form = lui.e.form(id='login')
+        # the submit event handler
         form.onsubmit = lui.a.select(element=form).submit(actor="login", routine="verify")
         return
      

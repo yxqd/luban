b2ec6ccee341b43fc6ffba4c1ae691e9c60beaf5
Jiao Lin <linjiao@caltech.edu>
2011-08-21 22:36:42 -0700

    modified:   luban/__init__.py: a bit of docstring

--- luban/__init__.py
+++ luban/__init__.py
@@ -12,6 +12,18 @@
 #
 
 
+"""
+luban: generic user interface specification
+
+packages:
+
+* ui: luban core. type system. element and action representations
+* weaver: convert luban ui representation to implementations such as javascript
+* utils: miscellaneous tools
+* timber: default extension of luban core
+
+"""
+
 # settings
 import os
 # whether pyre is available
@@ -19,6 +31,7 @@ has_pyre = os.environ.get('LUBAN_HAS_PYRE') or os.environ.get('HAS_PYRE')
 # allow extension to override element/action definitions without
 # throwing exceptions
 extension_allow_override = False
+
 
 # End of file 
 

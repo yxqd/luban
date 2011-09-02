9eba4cfcee73a2de53ec08b4959eb229f29a1ff2
Jiao Lin <linjiao@caltech.edu>
2011-09-01 22:14:03 -0700

    renamed:   timber/luban.timber/weaver/web/javascripts/luban/ => timber/luban.timber/weav$

--- core/luban/__init__.py
+++ core/luban/__init__.py
@@ -25,10 +25,10 @@ packages:
 
 # settings
 import os
-# whether pyre is available
+# .. whether pyre is available
 has_pyre = os.environ.get('LUBAN_HAS_PYRE') or os.environ.get('HAS_PYRE')
-# allow extension to override element/action definitions without
-# throwing exceptions
+# .. allow extension to override element/action definitions without
+# .. throwing exceptions
 extension_allow_override = False
 
 
--- timber/luban.timber/__init__.py
+++ timber/luban.timber/__init__.py
@@ -1,4 +1,27 @@
+# -*- Python -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                   Jiao Lin
+#                      California Institute of Technology
+#                      (C) 2006-2011  All Rights Reserved
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
 
+
+# ************************************************************
+# bad bad
 import luban
 luban.__doc__ += """* timber: default extension of luban core
 """
+# ************************************************************
+
+
+# activate extensions
+from . import elements
+
+
+# End of file 

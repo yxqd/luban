8c908a7810a5100363b627ea7f62bc4d21bb4253
Jiao Lin <linjiao@caltech.edu>
2011-09-15 13:34:25 -0700

    removed:   core/aokuang/aokuang/core/actors/button.py: this one is trivial; code moved inside helloworld.py

--- core/aokuang/aokuang/core/actors/helloworld.py
+++ core/aokuang/aokuang/core/actors/helloworld.py
@@ -45,5 +45,23 @@ class Actor(base):
         return frame
 
 
+    def button(self):
+        frame = lui.e.frame(title="luban test: button")
+        doc = frame.document(title="Click the following button")
+        b = doc.button(
+            label = 'button 1',
+            onclick = lui.a.alert('clicked button 1'),
+            )
+        b2 = doc.button(
+            label = 'button 2',
+            onclick = lui.a.load(actor=self.name, routine='onb2'),
+            )
+        return frame
+
+
+    def onb2(self, **kwds):
+        return lui.a.alert("clicked button 2")
+
+
 # End of file 
 

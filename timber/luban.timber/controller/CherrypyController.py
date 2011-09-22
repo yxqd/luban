d358d1d89b93d0b73364ed0c2cd30b96fdcf061b
Jiao Lin <linjiao@caltech.edu>
2011-09-21 23:09:56 -0700

    modified:   core/luban/controller/CherrypyController.py   timber/luban.timber/controller/CherrypyController.py
    "upload" method moved. it should not be in the core

--- timber/luban.timber/controller/CherrypyController.py
+++ timber/luban.timber/controller/CherrypyController.py
@@ -12,7 +12,31 @@
 #
 
 
-from luban.controller.CherrypyController import CherrypyController
+from luban.controller.CherrypyController import CherrypyController as base
+class CherrypyController(base):
+    
+    upload_limit = 1000*1024*1024 # 10M
+    upload_timeout = 60000 # 10 minutes
+    @cherrypy.expose
+    @cherrypy.tools.json_out(content_type="text/html")
+    def upload(self, myfile):
+        cherrypy.response.timeout = self.upload_timeout
+        size = 0
+        while True:
+            data = myfile.file.read(8192)
+            if not data:
+                break
+            size += len(data)
+            if size > self.upload_limit:
+                raise cherrypy.HTTPError(413)
+            continue
+                
+        return [{
+            "name": myfile.filename,
+            "size": size,
+            "type": str(myfile.content_type),
+            }]
+
 
 
 # End of file 

43dcc468b2496cbf001ecccb9eb5a9ecc61b31df
Jiao Lin <linjiao@caltech.edu>
2011-10-04 08:40:11 -0700

    added:   core/aokuang/aokuang/core/actors/loading/Actions.py: a demo that shows that it is ok to return muliple actions in a list

---
+++ core/aokuang/aokuang/core/actors/loading/Actions.py
@@ -0,0 +1,48 @@
+# -*- Python -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                   Jiao Lin
+#                      California Institute of Technology
+#                      (C) 2006-2011  All Rights Reserved
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+import luban
+
+from ....DemoPanelActor import Actor as base
+class Actor(base):
+
+    title='load two actions'
+    description=[
+        'In this example, two actions are loaded from the controller in one call,',
+        'and the loaded actions are then executed.',
+        'The loaded actions: adding a class to a document and an alert.',
+        ]
+
+    def createDemoPanel(self, **kwds):
+        container = luban.e.document()
+        doc = container.document(
+            title = 'The document to change',
+            id = 'document-to-change',
+            )
+        button = container.button(label = 'click me', id='load_actions_button')
+        # click the button will load from routine onloadaction of this actor.
+        button.onclick = luban.a.load(actor = self.name, routine = 'onloadaction')
+        return container
+    
+    
+    def onloadaction(self, **kwds):
+        # return two actions in a list
+        # both actions will be performed
+        return [
+            luban.a.select(id='document-to-change').addClass(cls='green-border'),
+            luban.a.alert("document class changed"),
+            ]
+
+
+# End of file 
+

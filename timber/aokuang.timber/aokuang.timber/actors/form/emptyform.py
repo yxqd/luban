a0b758791fc8c3f7b055b452f046c0734568ea56
Jiao Lin <linjiao@caltech.edu>
2011-10-08 10:37:03 -0700

    timber/aokuang.timber/aokuang.timber/actors/form/emptyform.py: trying to have a form full of all element types.

--- timber/aokuang.timber/aokuang.timber/actors/form/emptyform.py
+++ timber/aokuang.timber/aokuang.timber/actors/form/emptyform.py
@@ -20,7 +20,63 @@ class Actor(base):
     description = [
         ]
     def createDemoPanel(self, **kwds):
-        return luban.e.form(title='login')
+        # form
+        form = luban.e.form(title='test form')
+
+        # fields
+        # .. check boxes
+        checkbox1 = form.checkbox(
+            name='boolvar1', label='check box 1', checked=False)
+
+        checkbox2 = form.checkbox(
+            name='boolvar2', label='check box 2', checked=True)
+
+        # .. radio box
+        choices = ['one', 'two', 'three']
+        # entries are a list of (value, description)
+        # in simple cases, value could be equal to description
+        entries = [(v,v) for v in choices]
+        rad = form.radio(
+            id='radiofield', label='radio', name='radiovar',
+            entries=entries, selection='two',
+            )
+
+        # .. selector
+        choices = ['alpha', 'beta', 'gamma']
+        # entries are a list of (value, description)
+        # in simple cases, value could be equal to description
+        entries = [(v,v) for v in choices]
+        sel = form.selector(
+            label='selector', name='selectorvar',
+            entries=entries,
+            selection='alpha')
+
+        # .. text field
+        textfield = form.text(label='textfield', name='textvar')
+        
+        # .. text area
+        textarea = form.textarea(label='textarea', name='textareavar')
+        
+        # .. password field
+        pwfield = form.password(label='password', name='password')
+
+        # fields with validators
+        floatfield = form.text(label='float variable', name='floatvar')
+        intfield = form.text(label='int variable', name='intvar')
+        
+        # submit button
+        submit = form.submitbutton(label='submit')
+
+        # action when form is submitted
+        # Note: The routine "process" is already defined in
+        # base class FormProcessorInterface,
+        # here we just need to override "_postProcessing".
+        form.onsubmit = select(element=form).submit(
+            actor = self.name,
+            routine = 'process')
+
+        return document
+
 
 
 # End of file 

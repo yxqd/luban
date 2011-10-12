c4d7d33b1c82c2f5930197cb482ea1b5a23b195c
Jiao Lin <linjiao@caltech.edu>
2011-10-12 04:28:29 -0700

    core/luban/_filesystem/__init__.py: minor (documentation)

--- core/luban/_filesystem/__init__.py
+++ core/luban/_filesystem/__init__.py
@@ -11,6 +11,7 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
+# data model for stuffs in a file system
 # this package should be moved into the pyre framework in the long run
 
 # version

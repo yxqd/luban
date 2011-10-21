314220cfe1f216e9259d455c760a29781326e88c
Jiao Lin <linjiao@caltech.edu>
2011-10-21 11:46:54 -0700

    luban.cli: added a template sub-pkg

---
+++ core/luban/cli/_template/__init__.py
@@ -0,0 +1,64 @@
+# -*- Python -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                   Jiao Lin
+#                      California Institute of Technology
+#                      (C) 2006-2011  All Rights Reserved
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+"""
+this is a template sub package to implement a command with sub-commands
+"""
+
+
+def run(mod, *args, **opts):
+    return mod.run(*args, **opts)
+
+
+def importActionHandler(action):
+    code = 'from . import %s' % action
+    exec(code)
+    mod = locals()[action]
+    return mod
+
+
+def parse_cmdline():
+    import sys
+    if len(sys.argv) == 2:
+        action = 'help'
+    else:
+        action = sys.argv[2]
+
+    if action in ['-h', '--help']:
+        action = 'help'
+
+    if action not in commands:
+        print ()
+        print ("Invalid command: %s" % action)
+        action = 'help'
+    
+    mod = importActionHandler(action)
+    args, kwds = mod.parse_cmdline()
+    return [mod]+args, kwds
+
+
+public_commands = [
+    'help',
+    'cmd1',
+    ]
+
+hidden_commands = [
+    ]
+
+
+commands = public_commands + hidden_commands
+
+
+# End of file 
+

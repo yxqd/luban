74aeafa378002ef5ca175c686f89a01024eee3b6
Jiao Lin <linjiao@caltech.edu>
2011-11-19 08:35:58 -0800

    core/tests/luban/controller/decorators.py: added decorators that allows   converting arguments to required types.

--- core/luban/controller/decorators.py
+++ core/luban/controller/decorators.py
@@ -17,5 +17,49 @@ def public(f):
     return f
 
 
+def typeconversion(func):
+    
+    if not hasattr(func,'__annotations__'): return method
+    
+    import inspect
+    argspec = inspect.getfullargspec(func)
+    
+    def convert( t, T ):
+        return T(t)
+        
+    def wrapper(*args, **kwds):
+        
+        if len(argspec.args) != len(args):
+            raise TypeError( "%s() takes exactly %s positional argument (%s given)"
+                           %(func.__name__,len(argspec.args),len(args)) )
+
+        newargs = []
+        for argname,t in zip(argspec.args, args):
+            if argname in func.__annotations__:
+                T = func.__annotations__[argname]
+                t = T(t)
+            newargs.append(t)
+            continue
+
+        r = func(*newargs, **kwds)
+        
+        if 'return' in func.__annotations__:
+            T = func.__annotations__['return']
+            r = T(r)
+            
+        return r
+
+    return wrapper
+
+
+def bool(s):
+    if s.lower() in ['0', 'false', 'off']: return False
+    return True
+
+def int(s):
+    import builtins
+    return builtins.int(s)
+
+
 # End of file 
 
---
+++ core/tests/luban/controller/decorators.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                   Jiao Lin
+#                      California Institute of Technology
+#                      (C) 2006-2011 All Rights Reserved 
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+import unittest
+class TestCase(unittest.TestCase):
+     
+    def test1(self):
+        from luban.controller import decorators
+        @decorators.typeconversion
+        def f(b: decorators.bool=None, i: decorators.int=None):
+            print (b,i)
+            return
+        f('false', '10')
+        return
+     
+    
+    def test2(self):
+        from luban.controller import decorators
+        class A:
+            @decorators.typeconversion
+            def f(self, b: decorators.bool=None, i: decorators.int=None):
+                print (self, b,i)
+                return
+        A().f('false', '10')
+        return
+     
+    
+    def test3(self):
+        from luban.controller import decorators
+        class A:
+            @decorators.typeconversion
+            def f(self, b: decorators.bool=None, i: decorators.int=None, **kwds):
+                print (self, b,i, kwds)
+                return
+        A().f('false', '10', t=3)
+        return
+     
+    
+if __name__ == "__main__": unittest.main()
+    
+# End of file 

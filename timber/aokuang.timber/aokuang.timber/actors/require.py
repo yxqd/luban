d0780402b9277cff2af092b02295031753e9c7aa
Jiao Lin <linjiao@caltech.edu>
2011-11-20 22:20:13 -0800

    simplified the API for "require"

--- timber/aokuang.timber/aokuang.timber/actors/require.py
+++ timber/aokuang.timber/aokuang.timber/actors/require.py
@@ -16,6 +16,7 @@ import luban
 
 
 def emailform():
+    "frame to solicit from user his email"
     frame = luban.e.frame(title='input email')
     form = frame.form(title='email')
     form.text(label='email', name='email')
@@ -24,11 +25,13 @@ def emailform():
         actor='require', routine='onemailsubmission', 
         kwds=luban.event.data)
     return frame
-email_requirement = luban.decorators.Requirement()
 
 def check_email():
+    "a simple test that check if user email is available"
     email = luban.session.get('email')
     return not email
+
+email_requirement = luban.decorators.Requirement()
 email_requirement.check_requirement = check_email
 email_requirement.fullfill_requirement = emailform
 
@@ -39,12 +42,26 @@ class Actor(base):
     expose = 1
 
     def onemailsubmission(self, email=None, **kwds):
+        """handle email submission and establish "credential"
+        in this simple case, that is just the email
+        """
         luban.session['email'] = email
+        # this is actually established by the "requirement" decorator
+        # this will have problem if the user have multiple browser
+        # window(tab)s open and "authenticate" using the same
+        # requirement to different ports of the application.
+        # but that should be rare, and in fact it does not really
+        # matter -- it will look like to user just swapping the tabs
         onsuccess = luban.session['onsuccess']
         return onsuccess
+    
 
-    @luban.decorators.require(email_requirement, luban.a.load(actor='require', routine='showinfo', replaceinterface=1))
+    @luban.decorators.require(
+        # this is the requirement that before the real functinality can be invoked
+        email_requirement
+        )
     def showinfo(self, *args, **kwds):
+        "real functionality after 'authentication' is done"
         email = luban.session['email']
         frame = luban.e.frame(title='aokuang.timber: requirement')
         frame.paragraph(text = 'email: %s' % email)
--- timber/luban.timber/controller/decorators.py
+++ timber/luban.timber/controller/decorators.py
@@ -19,11 +19,13 @@ class Requirement:
     check_requirement = None # function to check whether a requirement is satisfied. return True if requirement is not fullfilled.
     fullfill_requirement = None # factory method to return a UI frame that solicit answers from user to fullfill the requirement
 
-def require(requirement, onsuccess):
+def require(requirement, actorname=None, onsuccess=None):
     """
     requirement: Requirement instance
     onsuccess: if requirement was succesfully fullfilled, take this action
     """
+    onsuccess_action = onsuccess
+    import inspect
     def convert(f):
         def newhandler(self, *args, **kwds):
             if not requirement.check_requirement():
@@ -33,11 +35,33 @@ def require(requirement, onsuccess):
                 else:
                     return luban.a.establishInterface(frame)
             
+            # this is the action to load the real functionality.
+            # it always need to be a load action.
+            # "actor" should be the name of the actor
+            # "routine" should be the name of this routine
+            # "replaceinterface" is required to replace the requirement solicitation inteface with the actual interface
+            onsuccess = onsuccess_action or luban.a.load(
+                actor = actorname or getactorname(
+                    inspect.getmodule(f).__name__, self.controller.actor_packages),
+                routine= f.__name__,
+                replaceinterface=1
+                )
             luban.session['onsuccess'] = onsuccess
             frame = requirement.fullfill_requirement()
             return luban.a.establishInterface(frame)
         return newhandler
     return convert
+
+
+def getactorname(modulename, packages):
+    """getactorname("myui.actors.hello", ['myui.actors']) -> 'hello'
+    """
+    for pkg in packages:
+        if modulename.startswith(pkg):
+            return modulename[len(pkg)+1:]
+        continue
+    return
+
 
 from luban import decorators
 decorators.Requirement = Requirement

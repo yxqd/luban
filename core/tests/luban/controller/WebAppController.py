6105eee2a977697018711bed9d26a86c131b64a4
Jiao Lin <linjiao@caltech.edu>
2011-12-17 10:04:18 -0800

    added a test case luban.controller.WebAppController. added directory core/tests/luban/controller to merlin

--- core/tests/luban/controller/WebAppController.py
+++ core/tests/luban/controller/WebAppController.py
@@ -16,8 +16,8 @@ import unittest
 class TestCase(unittest.TestCase):
      
     def test1(self):
-        from luban.controller.ControllerBase import ControllerBase
-        c = ControllerBase("actors")
+        from luban.controller.WebAppController import WebAppController
+        c = WebAppController(url='/controller', html_base='/', actor_package="actors")
         c._retrieveActor('start')
         return
      

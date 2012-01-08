41aaae3bff90d5870b868546ce100ca5b521b805
Jiao Lin <linjiao@caltech.edu>
2012-01-08 13:21:25 -0800

    workflows/aokuang.workflows/aokuang.workflows/actors/login.py: add some docstr

--- workflows/aokuang.workflows/aokuang.workflows/actors/login.py
+++ workflows/aokuang.workflows/aokuang.workflows/actors/login.py
@@ -21,24 +21,31 @@ class Actor(workflow.Actor):
 
     @luban.decorators.frameHandler
     def default(self):
-        frame = luban.e.frame(title='test')
-        
+        # give this context a name
         context = 'main-gate'
-        
+        # build the action in case of successful authentication
         onsuccess = luban.a.load(actor = self.name, routine = 'onsuccess')
+        # and save the context in the session
         luban.session[context] = {
             'onsuccess': onsuccess,
             }
-        form = workflow.visuals.form(context=context)
         
+        # when constructing the form, make sure
+        # that it knows in which context this form is
+        # so that after authentication, correct action will
+        # be carried out
+        form = workflow.visuals.form(context=context)
+
+        #
+        frame = luban.e.frame(title='test')
         frame.append(form)
         return frame
-
-
+    
+    
     def onsuccess(self, **kwds):
         welcome = luban.e.document(title='Welcome')
         return luban.a.select(id='').replaceContent(newcontent=welcome)
-
+    
     pass
 
 

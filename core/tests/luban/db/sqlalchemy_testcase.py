68d061bda8c77a08abd953dd6160f20bd535670b
Jiao Lin <linjiao@caltech.edu>
2012-01-15 17:30:33 -0800

    core/luban/db/sqlalchemy.py: fixed a bug reported by Adrian Klaver -- it shows up at python 2.6 due to relative imports

---
+++ core/tests/luban/db/sqlalchemy_testcase.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                                   Jiao Lin
+#                      California Institute of Technology
+#                      (C) 2006-2011 All Rights Reserved 
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+import unittest
+
+class TestCase(unittest.TestCase):
+     
+    def test1(self):
+        """luban.db.sqlalchemy"""
+        from luban.db import sqlalchemy
+        return
+     
+    
+def main():
+    unittest.main()
+    return
+    
+    
+if __name__ == "__main__":
+    main()
+    
+# version
+__id__ = "$Id$"
+
+# End of file 

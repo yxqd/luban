37997eae64afb87c0808275a400af7b32d3d5309
Jiao Lin <jiao.lin@gmail.com>
2012-05-18 13:46:45 +0000

    core/luban/AppConfig.py: trying a more gentle way

--- core/luban/AppConfig.py
+++ core/luban/AppConfig.py
@@ -11,6 +11,10 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
+
+import warnings
+
+
 class Option:
 
     def __init__(self, name, doc, example):
@@ -29,12 +33,12 @@ class Option:
 
 
 class AppConfig:
-
+    
     def __init__(self):
         self.options = {}
         return
     
-
+    
     def register(self, name, doc, example):
         opt = Option(name, doc, example)
         self.options[name] = opt
@@ -49,12 +53,13 @@ class AppConfig:
         try:
             import luban_app_config
         except ImportError:
-            raise RuntimeError("luban app configuration not found")
+            msg = "luban app configuration not found"
+            warnings.warn(msg)
+            # raise RuntimeError(msg)
+            return self._retrieveDefaultValue(name)
+        
         # check option definition
-        opt = self.options.get(name)
-        if opt is None:
-            msg = "Unknown option %r. Before using an option, developer should register the option." % name
-            raise RuntimeError(msg)
+        opt = self._checkOpt(name)
         
         try:
             v = getattr(luban_app_config, name)
@@ -62,8 +67,25 @@ class AppConfig:
             import os; modfile = os.path.abspath(luban_app_config.__file__)
             msg = "Missing option...\n\nPlease assign value to option %r in %s\n\nWhat it is:\n  %s\n\nExample\n  %s = %r\n" % (
                 name, modfile, opt.doc, name, opt.example)
-            raise RuntimeError(msg)
+            warnings.warn(msg)
+            return self._retrieveDefaultValue(name)
+            # raise RuntimeError(msg)
         return toobj(v)
+
+
+    
+    def _checkOpt(self, name):
+        # check option definition
+        opt = self.options.get(name)
+        if opt is None:
+            msg = "Unknown option %r. Before using an option, developer should register the option." % name
+            raise RuntimeError(msg)
+        return opt
+
+    
+    def _retrieveDefaultValue(self, name):
+        opt = self._checkOpt(name)
+        return toobj(opt.example)
 
 
 class RecurseStruct:
